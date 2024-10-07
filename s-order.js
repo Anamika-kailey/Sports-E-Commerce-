811a30f999eb46683022a583657bc3dc0a0be7ce
Anamika-kailey <anamikakailey04@gmail.com>
2024-10-07 23:10:09 +0530

    first commit

---
+++ s-order.js
@@ -0,0 +1,6 @@
+document.getElementById("login-btn").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="flex";
+})
+document.getElementById("close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none";
+})
